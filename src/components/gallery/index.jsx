a357c2df7b12f8f80d9d86f015dc8011f1f3f145
kevinaragn <kevinmartinaragon@gmail.com>
2024-09-25 03:56:21 -0300

    galeria casi terminada

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -1,49 +1,81 @@
-import React, { useState } from 'react';
-import './styles.css'; // Asegúrate de tener estilos personalizados
+import React, { useState, useEffect } from 'react';
+import { FaArrowLeft, FaArrowRight, FaTimes } from 'react-icons/fa';
+import './styles.css'; // Asegúrate de importar los estilos necesarios
 
 const Gallery = ({ images, onClose }) => {
-  const [selectedYear, setSelectedYear] = useState(Object.keys(images[0]?.years || [])[0]);
-  const [selectedImages, setSelectedImages] = useState(images[0]?.years[selectedYear] || []);
+  const [selectedYearIndex, setSelectedYearIndex] = useState(0);
+  const years = [...new Set(images.flatMap((img) => Object.keys(img.years)))];
+  const [selectedImages, setSelectedImages] = useState(images[0]?.years[years[0]] || []);
+  const sortedYears = years.sort((a, b) => parseInt(a) - parseInt(b));
 
-  // Agrupar imágenes por año
-  const years = [...new Set(images.flatMap(img => Object.keys(img.years)))];
+  // Controlar qué imagen está actualmente mostrando su fade in
+  const [fadeInKey, setFadeInKey] = useState(0);
 
-  const handleYearChange = (year) => {
-    setSelectedYear(year);
-    setSelectedImages(images.find(img => img.years[year])?.years[year] || []);
+  const handleYearChange = (index) => {
+    setSelectedYearIndex(index);
+    setSelectedImages(images.find((img) => img.years[sortedYears[index]])?.years[sortedYears[index]] || []);
   };
 
+  const goToPreviousYear = () => {
+    if (selectedYearIndex > 0) {
+      handleYearChange(selectedYearIndex - 1);
+    }
+  };
+
+  const goToNextYear = () => {
+    if (selectedYearIndex < sortedYears.length - 1) {
+      handleYearChange(selectedYearIndex + 1);
+    }
+  };
+
+  // Cada vez que cambien las imágenes, reiniciamos el efecto fade-in
+  useEffect(() => {
+    // Incrementamos el key para forzar la reactivación del efecto
+    setFadeInKey((prevKey) => prevKey + 1);
+  }, [selectedImages, selectedYearIndex]);
+
   return (
-    <div className='layout'>
     <div className="gallery-overlay">
       <div className="gallery-container">
-        <button className="close-button" onClick={onClose}>Cerrar</button>
+        <button className="close-button" onClick={onClose}>
+          <FaTimes size={30} />
+        </button>
+
+        <div className="image-navigation">
+          <button className="nav-button left" onClick={goToPreviousYear} disabled={selectedYearIndex === 0}>
+            <FaArrowLeft size={30} />
+          </button>
+
+          <div className="image-display">
+            {selectedImages.map((image, index) => (
+              <div
+                key={`${fadeInKey}-${index}`} // Key dinámica para reactivar el fade in siempre
+                className="image-item fade-in-active"
+              >
+                <img src={image.src} alt={`Imagen del año ${sortedYears[selectedYearIndex]}`} className="gallery-image" />
+                {image.description && <p className="image-description">{image.description}</p>}
+              </div>
+            ))}
+          </div>
+
+          <button className="nav-button right" onClick={goToNextYear} disabled={selectedYearIndex === sortedYears.length - 1}>
+            <FaArrowRight size={30} />
+          </button>
+        </div>
+
         <div className="year-selector">
-          {years.map(year => (
+          {sortedYears.map((year, index) => (
             <button
               key={year}
-              onClick={() => handleYearChange(year)}
-              className={selectedYear === year ? 'active' : ''}
+              onClick={() => handleYearChange(index)}
+              className={selectedYearIndex === index ? 'active' : ''}
             >
               {year}
             </button>
           ))}
         </div>
-        <div className="image-grid">
-          {selectedImages.map((image, index) => (
-            <div key={index} className="image-item">
-              <img src={image.src} alt={`Imagen del año ${selectedYear}`} />
-              <div className="image-description-container">
-                <p className="image-description">{image.description}</p>
-              </div>
-            </div>
-            
-          ))}
-        </div>
       </div>
     </div>
-    </div>
-    
   );
 };
 
--- src/components/mapComponent/index.jsx
+++ src/components/mapComponent/index.jsx
@@ -17,16 +17,53 @@ const markersData = [
     galleryData: [
       {
         years: {
-          '2019': [
-            { src: '/images/iglesia/2019.jpg', description: 'Iglesia en 2019' },
+          '1895': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727215701/1881-1895_rt4kyy.jpg',
+              
+              description: `Este cuadro muestra la primera capilla del viejo partido de General Sarmiento y,
+                              al lado de ella el templo parroquial cuando lo estaban construyendo.
+                              Esta capilla se inauguro con gran pompa el 8 de mayo de 1881.
+                              En el predio lindero el 8 de octubre de 1893, se puso la piedra fundamental del nuevo templo,
+                              que fue inaugurado el 29 de septiembre de 1895, coincidiendo con las fiestas patronales,
+                              cuando aun su paredes no estaban revocadas ni tenia los pisos envaldosados.` },
           ],
-          '2021': [
-            { src: '/images/iglesia/2021.jpg', description: 'Iglesia en 2021 lorem ipsum' },
+          '1900': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727215703/Captura_de_pantalla_2024-09-18_170022_xzvqa0.png',
+               description: '' },
           ],
+          '1937': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727215702/19399_pj6asl.jpg',
+               description: '' },
+          ],
+          '1939': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727215701/1939_sfjjws.jpg',
+               description: 'Interior de la Catedral' },
+          ],
+          
+          '1965': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727215702/1965_waw62y.jpg',
+               description: '' },
+          ],
+          '1980-1990': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727215703/Captura_de_pantalla_2024-09-18_172242_fe6us0.png',
+               description: '' },
+          ],
+          
+          
+
         }
       }
     ]
   },
+
+
+
+
+
+
+
+
+
   {
     position: [690, 3700],
     iconUrl: '/marker.png',
@@ -35,11 +72,67 @@ const markersData = [
     galleryData: [
       {
         years: {
-          '1970': [
-            { src: '/images/plazaSm/1970.jpg', description: 'Plaza de San Miguel en 1970' },
+          '1916': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216172/retrato_de_aprox_1895_uvux04.jpg',
+              description: `Al momento de crearse el Partido de Gral. Sarmiento no existía en toda la Provincia de Buenos Airess un monumento homenaje a Sarmiento. 
+                              El 01 de abril de 1911, en un gran acto público, al cual concurrieron los nietos de Sarmiento, Augusto y Eugenia, se coloco la piedra fundamental, siendo oficialmente inaugurado el monumento el día 09 de julio de 1916, el homenaje a Sarmiento, junto a varios cañones auténticos de la Guerra contra el Paraguay` },
           ],
-          '2020': [
-            { src: '/images/plazaSm/2020.jpg', description: 'Plaza de San Miguel en 2020' },
+          '1924': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216166/1924_urajzg.jpg', 
+              description: '' },
+          ],
+          '1925': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216166/1925_yv42xe.jpg', 
+              description: 'Estas imágenes reflejan una actividad política en la plaza de San Miguel. Es una concentración de radicales personalistas, el 20 de septiembre de 1925. Los seguidores de Yrigoyen se manifestaban en contra de las intenciones del gobierno nacional -en manos del radical antipersonalista Marcelo T. de Alvear- de intervenir la provincia de Buenos Aires. Se trataba de un reducto yrigoyenista, fundamental para las elecciones que se venían.' },
+          ],
+         
+          '1935': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216167/1935_jzebbk.jpg', 
+              description: '' },
+          ],
+          '1936': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727242245/1936_mftlad.jpg', 
+              description: '' },
+          ],
+          '1937': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216169/344762171_1174274057294945_4792117358108584642_n_avbuub.jpg', 
+              description: '' },
+          ],
+          '1944': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216167/1944_gff6lr.jpg', 
+              description: '' },
+            ],
+            "1950": [
+              { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727240383/decada_30_rdcsbs.jpg', 
+                description: '' },
+              ],
+          '1961': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216167/1961_Diario_la_Nacion_o6ovsl.jpg', 
+              description: 'Fotografía Diario La Nación' },
+          ],
+          '1965': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216166/153598986_3810775175677675_5160352493924135732_n_t4jz0l.jpg', 
+              description: '' },
+          ],
+          "1960's": [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216168/226629081_352215573105368_5403248384179603617_n_sq4g0q.jpg', 
+              description: '' },
+          ],
+          '1971': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216167/1971_plisyo.jpg', 
+              description: '' },
+          ],
+          '1976': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216170/431590841_7446694365419053_5830366128241073913_n_llkhfk.jpg', 
+              description: '' },
+          ],
+          '1980': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727244284/1980_dvctoh.jpg', 
+              description: '' },
+          ],
+          '1994': [
+            { src: 'https://res.cloudinary.com/dhc0kdmfp/image/upload/v1727216167/1994_u13qlj.jpg', 
+              description: '' },
           ],
         }
       }
